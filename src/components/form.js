8892457ac7316e4d0fe81758f52a9888e39befa1
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2021-06-24 16:23:13 -0600

    basic form done

---
+++ src/components/form.js
@@ -0,0 +1,27 @@
+
+const Form = (props) => {
+    return (
+        <form className="form-css">
+          <div className="">
+            <label htmlFor='name'>Your Name</label>
+            <input
+              type='text'
+              id='name'
+            />
+          </div>
+          <div>
+            <label htmlFor='email'>Your E-Mail</label>
+            <input
+              type='email'
+              id='email'
+            />
+          </div>
+          <div>
+            <button>Submit</button>
+          </div>
+        </form>
+      );
+    };
+
+    export default Form;
+    
